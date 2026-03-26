9f05afdae2fbe6737fd561d8c8f38231988ef1e7
Leonardo <lptleo11@gmail.com>
2026-03-25 21:15:24 -0300

    feat: adicionada entidade InsumoLote com propriedades e relação com a entidade Lote.

---
+++ backend/src/entities/InsumoLote.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Lote } from "./Lote";
+
+@Entity("insumos_lote")
+export class InsumoLote {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", nullable: false })
+  nome_insumo!: string;
+
+  @Column({ type: "varchar", nullable: false })
+  codigo_insumo!: string;
+
+  @Column({ type: "varchar", nullable: false })
+  lote_fornecedor!: string;
+
+  @Column({ type: "decimal", precision: 10, scale: 2, nullable: false })
+  quantidade_usada!: number;
+
+  @Column({ type: "varchar", nullable: false })
+  unidade_medida!: string;
+
+  @ManyToOne(() => Lote, (lote) => lote.insumos)
+  lote!: Lote
+}
