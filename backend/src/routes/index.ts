c4019b43194b29ebcef24c18e46f9fc0d242d57a
Leonardo <lptleo11@gmail.com>
2026-04-05 18:44:26 -0300

    feat: Implementando rotas dos módulos de lote, insumos, rastreabilidade e inspeção.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import usuarioRoutes from "./usuario.routes.js";
 import produtoRoutes from "./produto.routes.js";
+import loteRoutes from "./lote.routes.js";
+import insumoRoutes from "./insumoLote.routes.js";
+import rastreabilidadeRoutes from "./rastreabilidade.routes.js";
+import inspecaoRoutes from "./inspecaoLote.routes.js";
 
 const routes = Router();
 
 routes.use('/usuarios', usuarioRoutes);
 routes.use('/produtos', produtoRoutes);
+routes.use('/lotes', loteRoutes);
+routes.use('/insumos', insumoRoutes);
+routes.use('/rastreabilidade', rastreabilidadeRoutes);
+routes.use('/inspecao', inspecaoRoutes);
 
 export default routes;
