af26c94bca7003cd8793731715b2a40456884a10
Leonardo <lptleo11@gmail.com>
2026-04-06 00:31:55 -0300

    feat: Atualizando rotas e adicionando mergeParams em router no inspecaoLote.routes.ts.

--- backend/src/routes/inspecaoLote.routes.ts
+++ backend/src/routes/inspecaoLote.routes.ts
@@ -6,12 +6,12 @@ import { registrarInspecaoSchema } from "../dto/inspecaoLote.dto.js";
 import { roleGuard } from "../middlewares/roleGuard.js";
 import { PerfilUsuario } from "../entities/Usuario.js";
 
-const inspecaoRoutes = Router();
+const inspecaoRoutes = Router({ mergeParams: true });
 const inspecaoController = new InspecaoLoteController();
 
 inspecaoRoutes.use(authGuard);
 
-inspecaoRoutes.post("/lotes/:loteId/inspecao", roleGuard(PerfilUsuario.INSPETOR), validateBody(registrarInspecaoSchema), inspecaoController.registrar);
-inspecaoRoutes.get("/lotes/:loteId/inspecao", roleGuard(PerfilUsuario.OPERADOR, PerfilUsuario.INSPETOR, PerfilUsuario.GESTOR), inspecaoController.buscarPorLote);
+inspecaoRoutes.post("/", roleGuard(PerfilUsuario.INSPETOR), validateBody(registrarInspecaoSchema), inspecaoController.registrar);
+inspecaoRoutes.get("/", roleGuard(PerfilUsuario.OPERADOR, PerfilUsuario.INSPETOR, PerfilUsuario.GESTOR), inspecaoController.buscarPorLote);
 
 export default inspecaoRoutes;
