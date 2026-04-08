b45cbcc92f6354e2b6f29dbf5001b3c31845170a
Leonardo <lptleo11@gmail.com>
2026-04-07 22:34:15 -0300

    feat: Implementando lógica de autenticação no módulo de login.

--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log(this.email, this.password);
+    this.authService.login({ email: this.email, senha: this.password }).subscribe({
+      next: () => {
+        console.log('Login realizado com sucesso');
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        this.email = '';
+        this.password = '';
+
+        this.router.navigate(['/app/dashboard']);
+      }
+    })
+  }
+}
