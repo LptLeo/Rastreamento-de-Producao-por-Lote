bcaddb0a4783b63654ebe43ffa577cbabe156128
Leonardo <lptleo11@gmail.com>
2026-04-10 18:03:33 -0300

    feat: Adicionando rotas de Lote, Rastreabilidade e Configurações.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
 import { Dashboard } from './features/dashboard/dashboard';
+import { Lote } from './features/lote/lote';
+import { Rastreabilidade } from './features/rastreabilidade/rastreabilidade';
+import { Configuracoes } from './features/configuracoes/configuracoes';
 import { authGuard } from './core/guards/auth/auth-guard';
 
 export const routes: Routes = [
@@ -17,6 +20,24 @@ export const routes: Routes = [
   {
     path: 'app/dashboard',
     component: Dashboard,
+    canActivate: [authGuard],
+    pathMatch: 'full'
+  },
+  {
+    path: 'app/lote',
+    component: Lote,
+    canActivate: [authGuard],
+    pathMatch: 'full'
+  },
+  {
+    path: 'app/rastreabilidade',
+    component: Rastreabilidade,
+    canActivate: [authGuard],
+    pathMatch: 'full'
+  },
+  {
+    path: 'app/configuracoes',
+    component: Configuracoes,
     canActivate: [authGuard],
     pathMatch: 'full'
   },
